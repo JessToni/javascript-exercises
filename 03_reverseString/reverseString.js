06e0d2048fa4bf096725db2c80885a5e16a89f16
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-15 21:38:54 +0800

    Set for loop and reverse variables

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string = "hey") {
+    let reversedString = "";
+    let reverseCount = -1;
+    
+    for (let i = 0; i < string.length; i++) {
+        reversedString += string[reverseCount];
+        reverseCount--;
+    }
 
+    return reversedString;
 };
 
 // Do not edit below this line

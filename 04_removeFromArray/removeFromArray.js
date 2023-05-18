2d26ac9016971ed5c09eda61d1689060ee0bed37
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 22:51:05 +0800

    Use advanced solution from solution js file

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,5 @@
 const removeFromArray = function(array, ...args) {
-    const newArray = [];
-
-    array.forEach((item) => {
-        if (!args.includes(item)) {
-            newArray.push(item);
-        }
-    });
-
-    return newArray;
+    return array.filter(val => !args.includes(val));
 };
 
 // Do not edit below this line

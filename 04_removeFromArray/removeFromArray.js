0a1dd68e93a7607ec03fd4cdea299af3a0452ff8
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 08:29:33 +0800

    Add return value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,6 +2,8 @@ const removeFromArray = function(array = [1, 2, 3, 4], findValue) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] === findValue) {
             array.splice(i, 1);
+            
+            return array;
         }
     }
 };

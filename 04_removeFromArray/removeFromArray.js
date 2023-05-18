4954d560ebfb0c66c71acc560009ba02f1c8fb4d
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 08:38:49 +0800

    Add another for loop for findValue2 parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,20 @@
 const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue || array[i] === findValue2) {
+        if (array[i] === findValue) {
             array.splice(i, 1);
 
             return array;
         }
     }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === findValue2) {
+            array.splice(i, 1);
+
+            return array;
+        }
+    }
+
 };
 
 // Do not edit below this line

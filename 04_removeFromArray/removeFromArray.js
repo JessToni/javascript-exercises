1674ea630944308f87b69ceae723fefcd52fda36
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 22:11:42 +0800

    Add two more parameters and loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,4 @@
-const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2) {
+const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2, findValue3, findValue4) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] === findValue) {
             array.splice(i, 1);
@@ -7,6 +7,18 @@ const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2) {
 
     for (let i = 0; i < array.length; i++) {
         if (array[i] === findValue2) {
+            array.splice(i, 1);
+        }
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === findValue3) {
+            array.splice(i, 1);
+        }
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === findValue4) {
             array.splice(i, 1);
         }
     }

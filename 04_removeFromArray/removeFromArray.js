4e04826cea3a047d271723d01b2aa24f6e7968b7
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 22:42:51 +0800

    Apply solution from solution folder

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,13 @@
-const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2, findValue3, findValue4) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue) {
-            array.splice(i, 1);
-        }
-    }
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue2) {
-            array.splice(i, 1);
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray,push(item);
         }
-    }
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue3) {
-            array.splice(i, 1);
-        }
-    }
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue4) {
-            array.splice(i, 1);
-        }
-    }
+    });
 
     return array;
-
 };
 
 // Do not edit below this line

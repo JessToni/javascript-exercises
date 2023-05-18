6e0485e2338626fb6ea625c3380a618fc5bdb117
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 08:28:26 +0800

    Use for loop to findValue and splice it from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function(array = [1, 2, 3, 4], index) {
-    array.splice((index - 1),1);
+const removeFromArray = function(array = [1, 2, 3, 4], findValue) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === findValue) {
+            array.splice(i, 1);
+        }
+    }
 };
 
 // Do not edit below this line

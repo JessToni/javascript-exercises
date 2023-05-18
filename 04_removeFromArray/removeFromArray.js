c0470055f7a8855b05f9078809192e0880747905
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-18 08:32:14 +0800

    Add third parameter to removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
-const removeFromArray = function(array = [1, 2, 3, 4], findValue) {
+const removeFromArray = function(array = [1, 2, 3, 4], findValue, findValue2) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === findValue) {
+        if (array[i] === findValue || array[i] === findValue2) {
             array.splice(i, 1);
-            
+
             return array;
         }
     }

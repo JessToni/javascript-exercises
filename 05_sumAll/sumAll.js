49c9d83a2a324c9fe2b34c8abda429c5889c581d
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-19 11:07:57 +0800

    Replace parameters and add if condition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function(start, end) {
+const sumAll = function(one, two) {
     let total = 0;
+    let start = one;
+    let end = two;
+
+    if (one > two) {
+        start = two;
+        end = one;
+    }
 
     for (let i = start; i <= end; i++) {
         total += i;

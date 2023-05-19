c7539ea18fd79e46c8c8126be28e2028794be76a
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-19 10:59:51 +0800

    Return total after loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,11 @@
 const sumAll = function(start, end) {
     let total = 0;
+
     for (let i = start; i <= end; i++) {
         total += i;
     }
+
+    return total;
 };
 
 // Do not edit below this line

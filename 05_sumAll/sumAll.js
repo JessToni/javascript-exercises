c7cb36b7d2cf02977d38600b8a5815cd9beae83d
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-19 10:58:14 +0800

    Loop the addition of a range of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let total = 0;
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
 };
 
 // Do not edit below this line

c41c14052c9016653c360d7169cdc8f987d4e22c
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-22 09:16:58 +0800

    Add calculation for each F to C and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convert = (fahrenheit - 32) * (5/9);
+  return convert;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convert = (celsius * (9/5)) + 32;
+  return convert;
 };
 
 // Do not edit below this line

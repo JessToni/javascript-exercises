4e373323f65c28de088c280e0f131d7f2353f6fa
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-05-22 09:25:53 +0800

    Round converted value to first decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
 const convertToCelsius = function(fahrenheit) {
   let convert = (fahrenheit - 32) * (5/9);
-  return convert;
+  let roundToDecimal = Math.round(convert * 10) / 10;
+  return roundToDecimal;
 };
 
 const convertToFahrenheit = function(celsius) {
   let convert = (celsius * (9/5)) + 32;
-  return convert;
+  let roundToDecimal = Math.round(convert * 10) / 10;
+  return roundToDecimal;
 };
 
 // Do not edit below this line

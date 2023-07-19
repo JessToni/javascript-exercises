5013098f3628c854aa5e21dc0c152d411a0e828f
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-19 22:34:12 +0800

    Split string into array and reverse to check for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let splitString = string.split("");
+    let reversedString = splitString.reverse();
 
+    if (reversedString === splitString) {
+        return true;
+    }
 };
 
 // Do not edit below this line

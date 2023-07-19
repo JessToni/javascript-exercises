574b5f5fa1385105ac06190995f62ba393e9d384
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-19 18:35:18 +0800

    Push number inputs into an array

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,4 +1,10 @@
 const add = function(a, b) {
+  if (a === null || a === "") {
+    a  = 0;
+  } else if (b === null || b === "") {
+    b = 0;
+  }
+  
 	let sum = a + b
   return sum
 };
@@ -8,8 +14,18 @@ const subtract = function(a, b) {
   return difference
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+	let sum = 0;
+  const arr = [];
+
+  //Place all numbers loaded in the function into an array
+  for (let number of numbers) {
+    arr.push(number);
+  }
+
+  /*for (let contents of arr) {
+
+  }*/
 };
 
 const multiply = function() {

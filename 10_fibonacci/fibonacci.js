8ff0da31db875fd7895c87ec6aeed92ba943f1a2
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-22 11:38:43 +0800

    Add a loop that pushes fibonacci numbers into an array

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let arr = [];
+    for (let i = 0; i < number; i++) {
+        if (arr.length <= 1) {
+            arr.push(1);
+        } else {
+            nextFib = arr[i-1] + arr[i-2];
+            arr.push(nextFib);
+            return arr[-1];
+        }
+    }
 };
 
 // Do not edit below this line

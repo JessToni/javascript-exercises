487011a714cd53407e06799b934c457f8cb803b8
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-22 11:06:54 +0800

    Add "" to split and join function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (string) {
     let changeString = string.toLowerCase().replace(/[^a-z]/g, "");
-    let reversedString = changeString.split().reverse().join();
+    let reversedString = changeString.split("").reverse().join("");
     return changeString == reversedString;
 };
 

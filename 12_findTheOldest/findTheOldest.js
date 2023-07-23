f3993bf20713eb7fddc1a389d3d36141744819f4
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-23 11:28:40 +0800

    Use sort function to find the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
 
+        return (lastPerson > nextPerson ? -1:1)
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line

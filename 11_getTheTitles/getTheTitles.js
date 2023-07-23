9b52542d940cfccc1b52bee99570a12e2c21561a
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-23 10:26:32 +0800

    Add a return statement after the for loop

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,11 @@
 const getTheTitles = function(books) {
     arr = [];
-    
+
     for (let book of books) {
         arr.push(book.title);
     }
+
+    return arr;
 };
 
 // Do not edit below this line

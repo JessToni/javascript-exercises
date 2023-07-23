a93a13a30f268069be014325f914e48e8c5cc90e
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-07-23 10:14:07 +0800

    Use forEach to iterate over array input

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    arr = [];
+    books.array.forEach(element => {
+        arr.push(books.title);
+    });
 };
 
 // Do not edit below this line
